3df062b51b9f428b1bd793419b222d6eff5b6381
halvee <halveeg@gmail.com>
2026-07-30 18:03:21 +0800

    docs: publish fixed onlyoffice desktop cmd installer

--- docs/install/onlyoffice-desktop-install.cmd
+++ docs/install/onlyoffice-desktop-install.cmd
@@ -26,9 +26,9 @@ mkdir "%PLUGIN_DIR%\resources\light" >nul 2>nul
 
 call :download "%BASE_URL%/onlyoffice/resources/light/icon.png" "%PLUGIN_DIR%\resources\light\icon.png" || exit /b 1
 call :download "%BASE_URL%/onlyoffice/resources/light/icon@2x.png" "%PLUGIN_DIR%\resources\light\icon@2x.png" || exit /b 1
-call :download "%BASE_URL%/onlyoffice/index.html?v=ms7btrp5" "%PLUGIN_DIR%\index.html" || exit /b 1
-call :download "%BASE_URL%/onlyoffice/settings.html?v=ms7btrp5" "%PLUGIN_DIR%\settings.html" || exit /b 1
-call :download "%BASE_URL%/onlyoffice/workflow-editor.html?v=ms7btrp5" "%PLUGIN_DIR%\workflow-editor.html" || exit /b 1
+call :download "%BASE_URL%/onlyoffice/index.html?v=ms7ccet9" "%PLUGIN_DIR%\index.html" || exit /b 1
+call :download "%BASE_URL%/onlyoffice/settings.html?v=ms7ccet9" "%PLUGIN_DIR%\settings.html" || exit /b 1
+call :download "%BASE_URL%/onlyoffice/workflow-editor.html?v=ms7ccet9" "%PLUGIN_DIR%\workflow-editor.html" || exit /b 1
 
 set "JS=%TEMP%\coco-onlyoffice-desktop-%RANDOM%.js"
 > "%JS%" echo var fs = new ActiveXObject('Scripting.FileSystemObject');
@@ -37,14 +37,39 @@ set "JS=%TEMP%\coco-onlyoffice-desktop-%RANDOM%.js"
 >> "%JS%" echo function writeText(file,text){ var s=new ActiveXObject('ADODB.Stream'); s.Type=2; s.Charset='utf-8'; s.Open(); s.WriteText(text); s.SaveToFile(file,2); s.Close(); }
 >> "%JS%" echo var files = ['index.html','settings.html','workflow-editor.html'];
 >> "%JS%" echo for (var i=0;i^<files.length;i++){ var p=dir+'\\'+files[i]; var html=readText(p); html=html.replace(/(["'])\.\/v1\//g,'$1../v1/').replace(/(["'])\.\/assets\//g,'$1'+base+'/onlyoffice/assets/'); if(api){ html=html.replace('</head>','  ^<script src="./coco-runtime-config.js"^>^</script^>^</head^>'); } writeText(p, html); }
->> "%JS%" echo var config = {name:'Coco',nameLocale:{zh:'Coco',en:'Coco'},guid:'asc.{8D9B5A2C-1F3E-4C7A-9B0D-2E6F1A4C8B30}',version:'0.0.1',minVersion:'7.0.0',variations:[{description:'Coco',descriptionLocale:{zh:'Coco'},url:'index.html',icons:['resources/light/icon.png','resources/light/icon@2x.png'],isViewer:true,EditorsSupport:['word','cell','slide'],isVisual:true,isModal:false,isInsideMode:true,initDataType:'none',initData:'',isUpdateOften:false,buttons:[],size:[320,600],events:[]}]};
->> "%JS%" echo writeText(dir+'\\config.json', JSON.stringify(config, null, 2));
 >> "%JS%" echo function jsString(value){ return String(value).replace(/\\/g,'\\\\').replace(/"/g,'\\"').replace(/\r/g,'\\r').replace(/\n/g,'\\n'); }
 >> "%JS%" echo if(api){ writeText(dir+'\\coco-runtime-config.js', 'window.__COCO_RUNTIME_CONFIG__ = {\r\n  "apiBaseUrl": "' + jsString(api) + '"\r\n};'); }
 cscript //nologo "%JS%" "%PLUGIN_DIR%" "%BASE_URL%" "%ASC_GUID%" "%API_BASE_URL%"
 set "RC=%ERRORLEVEL%"
 del "%JS%" >nul 2>nul
 if not "%RC%"=="0" exit /b %RC%
+
+> "%PLUGIN_DIR%\config.json" echo {
+>> "%PLUGIN_DIR%\config.json" echo   "name": "Coco",
+>> "%PLUGIN_DIR%\config.json" echo   "nameLocale": { "zh": "Coco", "en": "Coco" },
+>> "%PLUGIN_DIR%\config.json" echo   "guid": "%ASC_GUID%",
+>> "%PLUGIN_DIR%\config.json" echo   "version": "0.0.1",
+>> "%PLUGIN_DIR%\config.json" echo   "minVersion": "7.0.0",
+>> "%PLUGIN_DIR%\config.json" echo   "variations": [
+>> "%PLUGIN_DIR%\config.json" echo     {
+>> "%PLUGIN_DIR%\config.json" echo       "description": "Coco",
+>> "%PLUGIN_DIR%\config.json" echo       "descriptionLocale": { "zh": "Coco" },
+>> "%PLUGIN_DIR%\config.json" echo       "url": "index.html",
+>> "%PLUGIN_DIR%\config.json" echo       "icons": ["resources/light/icon.png", "resources/light/icon@2x.png"],
+>> "%PLUGIN_DIR%\config.json" echo       "isViewer": true,
+>> "%PLUGIN_DIR%\config.json" echo       "EditorsSupport": ["word", "cell", "slide"],
+>> "%PLUGIN_DIR%\config.json" echo       "isVisual": true,
+>> "%PLUGIN_DIR%\config.json" echo       "isModal": false,
+>> "%PLUGIN_DIR%\config.json" echo       "isInsideMode": true,
+>> "%PLUGIN_DIR%\config.json" echo       "initDataType": "none",
+>> "%PLUGIN_DIR%\config.json" echo       "initData": "",
+>> "%PLUGIN_DIR%\config.json" echo       "isUpdateOften": false,
+>> "%PLUGIN_DIR%\config.json" echo       "buttons": [],
+>> "%PLUGIN_DIR%\config.json" echo       "size": [320, 600],
+>> "%PLUGIN_DIR%\config.json" echo       "events": []
+>> "%PLUGIN_DIR%\config.json" echo     }
+>> "%PLUGIN_DIR%\config.json" echo   ]
+>> "%PLUGIN_DIR%\config.json" echo }
 
 echo Coco ONLYOFFICE Desktop local shell installed:
 echo   %PLUGIN_DIR%
